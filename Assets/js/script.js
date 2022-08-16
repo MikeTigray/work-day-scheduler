e0aef6e4980d52f7b278c37c04f82bd589bb09d3
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-08-16 10:48:51 -0400

    declared getItem variables and set all chores in diffferent keys inside local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,17 +12,32 @@ var pm2 = $("#2pm");
 var pm3 = $("#3pm");
 var pm4 = $("#4pm");
 var pm5 = $("#5pm");
-//declared variables for button
-var button1 = $("#btn-1");
-var button2 = $("#btn-2");
-// variables of chores retrieved from local storage
-var retrieved1 = am8.text(localStorage.getItem("chores"));
-// var retrieved2 = am9.text(localStorage.getItem("chores"));
+//declared variables for buttons
+var button8 = $("#btn-8");
+var button9 = $("#btn-9");
+var button10 = $("#btn-10");
+var button11 = $("#btn-11");
+var button12 = $("#btn-12");
+var button13 = $("#btn-13");
+var button14 = $("#btn-14");
+var button15 = $("#btn-15");
+var button16 = $("#btn-16");
+var button17 = $("#btn-17");
+// variables of chores retrieved from local storage/All chores are stored in different keys
+var retrieved8 = am8.text(localStorage.getItem("chores8"));
+var retrieved9 = am9.text(localStorage.getItem("chores9"));
+var retrieved10 = am10.text(localStorage.getItem("chores10"));
+var retrieved11 = am11.text(localStorage.getItem("chores11"));
+var retrieved12 = pm12.text(localStorage.getItem("chores12"));
+var retrieved13 = pm1.text(localStorage.getItem("chores13"));
+var retrieved14 = pm2.text(localStorage.getItem("chores14"));
+var retrieved15 = pm3.text(localStorage.getItem("chores15"));
+var retrieved16 = pm4.text(localStorage.getItem("chores16"));
+var retrieved17 = pm5.text(localStorage.getItem("chores17"));
 
 // array for all time variables
 var times = [am8, am9, am10, am11, pm12, pm1, pm2, pm3, pm4, pm5];
-// console.log(times);
-// localStorage.setItem("array", JSON.stringify(times));
+
 // a for loop to set past, present ,and future
 for (i = 0; i < times.length; i++) {
   if (times[i].attr("data-time") < today.hour()) {
@@ -33,48 +48,31 @@ for (i = 0; i < times.length; i++) {
     times[i].addClass("future");
   }
 }
-//array to put to local storage
-var object = {
-  input1: am8.val(),
-  input2: am9.val(),
-  input3: am10.val(),
-  input4: am11.val(),
-  input5: pm12.val(),
-  input6: pm1.val(),
-  input7: pm2.val(),
-  input8: pm3.val(),
-  input9: pm4.val(),
-  input10: pm5.val(),
-};
+//When save buttons are clicked....
+$(".saveBtn").on("click", function () {
+  var buttonId = $(this).attr("id");
 
-button1.on("click", function () {
-  localStorage.setItem("chores", am8.val());
+  if (buttonId == "btn-8") {
+    localStorage.setItem("chores8", am8.val());
+  } else if (buttonId == "btn-9") {
+    localStorage.setItem("chores9", am9.val());
+  } else if (buttonId == "btn-10") {
+    localStorage.setItem("chores10", am10.val());
+  } else if (buttonId == "btn-11") {
+    localStorage.setItem("chores11", am11.val());
+  } else if (buttonId == "btn-12") {
+    localStorage.setItem("chores12", pm12.val());
+  } else if (buttonId == "btn-13") {
+    localStorage.setItem("chores13", pm1.val());
+  } else if (buttonId == "btn-14") {
+    localStorage.setItem("chores14", pm2.val());
+  } else if (buttonId == "btn-15") {
+    localStorage.setItem("chores15", pm3.val());
+  } else if (buttonId == "btn-16") {
+    localStorage.setItem("chores16", pm4.val());
+  } else if (buttonId == "btn-17") {
+    localStorage.setItem("chores17", pm5.val());
+  } else return;
+
+  console.log(buttonId);
 });
-button2.on("click", function () {
-  localStorage.setItem("chores", am9.val());
-});
-
-console.log(object.input1);
-// button2.on("click", function () {
-//   localStorage.setItem("chores", am9.val());
-// });
-//   localStorage.setItem("chores", am9.val());
-//   localStorage.setItem("chores", am10.val());
-//   localStorage.setItem("chores", am11.val());
-//   localStorage.setItem("chores", pm12.val());
-//   localStorage.setItem("chores", pm1.val());
-//   localStorage.setItem("chores", pm2.val());
-//   localStorage.setItem("chores", pm3.val());
-//   localStorage.setItem("chores", pm4.val());
-//   localStorage.setItem("chores", pm5.val());
-
-// am8.val(retrieved);
-// am9.val(retrieved);
-// am10.val(retrieved);
-// am11.val(retrieved);
-// pm12.val(retrieved);
-// pm1.val(retrieved);
-// pm2.val(retrieved);
-// pm3.val(retrieved);
-// pm4.val(retrieved);
-// pm5.val(retrieved);
